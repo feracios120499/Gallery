9e86c60b995654db0354c22573ede27cd0aa84c6
feracios120499 <37274332+feracios120499@users.noreply.github.com>
2018-06-03 22:54:48 +0300

    win
    Галерея картинок. Написать программу для просмотра изображений.
    Пользователь выбирает папку, из которой нужно просмотреть картинки.
    Далее содержимое папки считывается (отобразить прогресс с помощью
    ProgressBar). Загружается первая картинка (размер регулируется Slider).
    Пользователь перелистывает картинки или может выбрать одну из них в
    ListBox. Информация о просмотре и файле картинки отображается в
    Expander.

--- Gallery/MainWindow.xaml.cs
+++ Gallery/MainWindow.xaml.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -20,9 +23,72 @@ namespace Gallery
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            width = image.Width;
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog folder = new FolderBrowserDialog();
+            if (folder.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var images = GetFiles(folder.SelectedPath, "*.jpg|*.png", SearchOption.TopDirectoryOnly);
+                int i = 1;
+                this.images.Children.Clear();
+                foreach (var item in images)
+                {
+                    i++;
+                    progressBar.Value = (100 * i)/images.Count();
+                    this.images.Children.Add(CreateImage(item));
+                    await Task.Delay(100);
+                }
+            }
+        }
+        private string[] GetFiles(string path, string searchPattern, SearchOption searchOption)
+        {
+
+            string[] searchPatterns = searchPattern.Split('|');
+            List<string> files = new List<string>();
+            foreach (string sp in searchPatterns)
+            {
+
+                try
+                {
+                    foreach (var item in System.IO.Directory.GetFiles(path, sp, searchOption))
+                    {
+                        files.Add(item);
+                    }
+                }
+                catch { }
+            }
+           
+            files.Sort();
+            progressBar.Value = 100;
+            return files.ToArray();
+        }
+        private Image CreateImage(string path)
+        {
+            Image image = new Image();
+            image.Source = new BitmapImage(new Uri(path));
+            image.Cursor = System.Windows.Input.Cursors.Hand;
+            image.MouseLeftButtonDown += Image_MouseLeftButtonDown;
+            
+            return image;
+        }
+
+        private void Image_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image selectedImage = sender as Image;
+            image.Source = selectedImage.Source;
+        }
+        double width = 350;
+        private void slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            image.Width = width * slider.Value / 10;
+            image.Stretch = Stretch.Fill;
         }
     }
 }
